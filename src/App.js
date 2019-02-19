b69db1a60676089c7c416538dde43003547e3a8c
wahyu alfarisi <wahyualfarisi@gmail.com>
2019-02-19 17:46:41 +0700

    commit #2

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-            <h1>Hello Project</h1>
+            <h1>Hello Project "this is development app"</h1>
         </header>
       </div>
     );

2fa132cb219e9e27a186bd6e45fb408ac11e61da
wahyu alfarisi <wahyualfarisi@gmail.com>
2019-02-19 17:53:11 +0700

    deploy

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-            <h1>Hello Project "this is development app ss"</h1>
+            <h1></h1>
         </header>
       </div>
     );

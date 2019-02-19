b2016be389ca7f1826ea45462dc371fb41f36ae2
wahyu alfarisi <wahyualfarisi@gmail.com>
2019-02-19 17:43:03 +0700

    Updates

---
+++ precache-manifest.bfcace9d5c268c4117c4f81982a354d4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e01a2b72839c566ee4f4",
+    "url": "/myreact/static/js/runtime~main.e01a2b72.js"
+  },
+  {
+    "revision": "012d4b3a5116a01af19f",
+    "url": "/myreact/static/js/main.012d4b3a.chunk.js"
+  },
+  {
+    "revision": "063ce5bc9f8a30d66648",
+    "url": "/myreact/static/js/2.063ce5bc.chunk.js"
+  },
+  {
+    "revision": "012d4b3a5116a01af19f",
+    "url": "/myreact/static/css/main.ff6bb288.chunk.css"
+  },
+  {
+    "revision": "3fe4039fef963bf8d5c45e8af9ebdb38",
+    "url": "/myreact/index.html"
+  }
+];
